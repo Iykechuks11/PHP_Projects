855acc5ab93aa55eb8eb853ad8ab5664b520859e
iykechuks11 <johnchamp1111@gmail.com>
2024-05-26 20:43:05 +0300

    Day 1 of learning PHP

---
+++ 01_output.php
@@ -0,0 +1,21 @@
+<?php
+/* -------- Output & Comments ------- */
+
+/*
+  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
+*/
+// echo 123, 'Hello';
+
+// print 123;
+
+// print_r('Hello Daddy.');
+
+// print_r([1, 2, 3]);
+
+// var_dump('Hello');
+
+// var_dump(true);
+
+// var_export('Hello');
+
+?>
